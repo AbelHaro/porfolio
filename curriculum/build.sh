4aebe3367d4f49f4ea5f5974998ffd01eaac89b6
Abel Haro <ahararm@upv.es>
2025-05-20 19:08:53 +0200

    feat: Agregar archivos PDF generados y actualizar script de compilación

--- curriculum/build.sh
+++ curriculum/build.sh
@@ -3,12 +3,12 @@
 # Compilar versión en español
 echo '\def\spanish{1}\input{cv_plantilla.tex}' > temp_es.tex
 TEXINPUTS=".:" pdflatex temp_es.tex
-mv temp_es.pdf cv_es.pdf
+mv temp_es.pdf abel_haro_armero_cv_es.pdf
 
 # Compilar versión en inglés
 echo '\input{cv_plantilla.tex}' > temp_en.tex
 TEXINPUTS=".:" pdflatex temp_en.tex
-mv temp_en.pdf cv_en.pdf
+mv temp_en.pdf abel_haro_armero_cv_en.pdf
 
 # Limpiar archivos temporales
 rm -f *.aux *.log *.out *.synctex.gz temp_*.tex

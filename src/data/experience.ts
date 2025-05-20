97ae09cecb240d02820f64d0060e9fb9bab43263
Abel Haro <ahararm@upv.es>
2025-05-20 20:02:42 +0200

    feat: agregar nuevos íconos SVG y actualizar la experiencia y secciones del portafolio

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -6,13 +6,13 @@ export interface Experience {
 
 export const experience: Experience[] = [
     {
-        title: "Práticas DISCA - UPV",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut nisi nec nunc.",
-        date: "Octubre 2024 - Julio 2025"
+        title: "Prácticas en el Departamento DISCA de la UPV",
+        description: "Desarrollo de un sistema de detección de defectos en objetos mediante imágenes, utilizando redes neuronales.",
+        date: "Octubre 2024 - presente"
     },
     {
-        title: "Prácticas Soltecsis S.L.",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut nisi nec nunc.",
+        title: "Prácticas en SOLTECSIS S.L.",
+        description: "Depuración y corrección de errores durante la migración del proyecto de código abierto FWCloud de JavaScript a TypeScript.",
         date: "Julio 2024"
     }
 ]
